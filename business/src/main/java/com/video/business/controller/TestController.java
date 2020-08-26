f8c3184379f2c5454b495b598665ddd604c0799e
gs <1623487989@qq.com>
2020-08-26 16:09:25 +0800

    增加business模块，并添加启动代码

---
+++ business/src/main/java/com/video/business/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.video.business.controller;
+
+
+import com.video.server.domain.Test;
+import com.video.server.service.TestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+//如果返回的是json数据用RestController（主要针对是接口），如果返回的是界面用Controller
+@RestController
+public class TestController {
+
+    @Resource
+    private TestService testService;
+
+    @RequestMapping("/test")
+    public List<Test> test(){
+
+        return testService.query();
+    }
+}
