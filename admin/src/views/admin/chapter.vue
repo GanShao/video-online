4ae51dea3caf1c8482b5ab2c98fcf0c87c05e1b3
gs <1623487989@qq.com>
2020-08-31 10:49:10 +0800

    增加刷新功能，代码格式化

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,103 +1,112 @@
 <template>
-  <table id="simple-table" class="table  table-bordered table-hover">
-    <thead>
-    <tr>
-      <th>ID</th>
-      <th>名称</th>
-      <th>课程ID</th>
-      <th>操作</th>
-    </tr>
-    </thead>
+  <div>
+    <p>
+      <button v-on:click="queryChapterPage()" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-refresh blue"></i>
+        刷新
+      </button>
+    </p>
 
-    <tbody>
-    <tr v-for="chapter in chapters">
-      <td>{{chapter.id}}}</td>
-      <td>{{chapter.name}}}</td>
-      <td>{{chapter.courseId}}}</td>
+    <table id="simple-table" class="table  table-bordered table-hover">
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>名称</th>
+        <th>课程ID</th>
+        <th>操作</th>
+      </tr>
+      </thead>
 
-      <td>
-        <div class="hidden-sm hidden-xs btn-group">
-          <button class="btn btn-xs btn-success">
-            <i class="ace-icon fa fa-check bigger-120"></i>
-          </button>
+      <tbody>
+      <tr v-for="chapter in chapters">
+        <td>{{chapter.id}}}</td>
+        <td>{{chapter.name}}}</td>
+        <td>{{chapter.courseId}}}</td>
 
-          <button class="btn btn-xs btn-info">
-            <i class="ace-icon fa fa-pencil bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-danger">
-            <i class="ace-icon fa fa-trash-o bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-warning">
-            <i class="ace-icon fa fa-flag bigger-120"></i>
-          </button>
-        </div>
-
-        <div class="hidden-md hidden-lg">
-          <div class="inline pos-rel">
-            <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-              <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+        <td>
+          <div class="hidden-sm hidden-xs btn-group">
+            <button class="btn btn-xs btn-success">
+              <i class="ace-icon fa fa-check bigger-120"></i>
             </button>
 
-            <ul
-              class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-              <li>
-                <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
+            <button class="btn btn-xs btn-info">
+              <i class="ace-icon fa fa-pencil bigger-120"></i>
+            </button>
+
+            <button class="btn btn-xs btn-danger">
+              <i class="ace-icon fa fa-trash-o bigger-120"></i>
+            </button>
+
+            <button class="btn btn-xs btn-warning">
+              <i class="ace-icon fa fa-flag bigger-120"></i>
+            </button>
+          </div>
+
+          <div class="hidden-md hidden-lg">
+            <div class="inline pos-rel">
+              <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
+                <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+              </button>
+
+              <ul
+                class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
+                <li>
+                  <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
 																			<span class="blue">
 																				<i class="ace-icon fa fa-search-plus bigger-120"></i>
 																			</span>
-                </a>
-              </li>
+                  </a>
+                </li>
 
-              <li>
-                <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
+                <li>
+                  <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
 																			<span class="green">
 																				<i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
 																			</span>
-                </a>
-              </li>
+                  </a>
+                </li>
 
-              <li>
-                <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
+                <li>
+                  <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
 																			<span class="red">
 																				<i class="ace-icon fa fa-trash-o bigger-120"></i>
 																			</span>
-                </a>
-              </li>
-            </ul>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </td>
-    </tr>
-    </tbody>
-  </table>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
     export default {
         name: "chapter",
-        data:function(){
-            return{
-                chapters:[]
+        data: function () {
+            return {
+                chapters: []
             }
         },
         mounted: function () {
             let _this = this;
-            _this.list();
+            _this.queryChapterPage();
             // sidebar激活样式方法一
             // this.$parent.activeSidebar("business-chapter-sidebar");
         },
         methods: {
-            list(){
+            queryChapterPage() {
                 let _this = this;
                 //post请求有两种方式：1、表单方式。2、json流的方式
                 //jQuery默认使用表单的方式，但是vue,angular使用流的方式
-                _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/queryChapterPage",{
-                    page:1,
-                    size:5
-                }).then((response)=>{
-                    console.log("查询大章列表",response);
+                _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/queryChapterPage", {
+                    page: 1,
+                    size: 5
+                }).then((response) => {
+                    console.log("查询大章列表", response);
                     _this.chapters = response.data.list;
                 })
 
