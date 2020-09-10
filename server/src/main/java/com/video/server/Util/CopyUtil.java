6598533fe2a3f5fa3db409340ee610e8fe3265b1
gs <1623487989@qq.com>
2020-09-10 10:42:44 +0800

    封装 BeanUtils.copyProperties()方法，简化实体类复制和列表复制的代码

---
+++ server/src/main/java/com/video/server/Util/CopyUtil.java
@@ -0,0 +1,39 @@
+package com.video.server.Util;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 封装 BeanUtils.copyProperties()方法，简化实体类复制和列表复制的代码
+ */
+public class CopyUtil {
+
+    public static <T> List<T> copyList(List source, Class<T> clazz) {
+        List<T> target = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(source)) {
+            for (Object c : source) {
+                T obj = copy(c, clazz);
+                target.add(obj);
+            }
+        }
+        return target;
+    }
+
+    public static <T> T copy(Object source, Class<T> clazz) {
+        if (source == null) {
+            return null;
+        }
+        T obj = null;
+        try {
+            obj = clazz.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        BeanUtils.copyProperties(source, obj);
+        return obj;
+    }
+}
+
--- server/src/main/java/com/video/server/service/ChapterService.java
+++ server/src/main/java/com/video/server/service/ChapterService.java
@@ -2,17 +2,16 @@ package com.video.server.service;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.video.server.Util.CopyUtil;
 import com.video.server.Util.UuidUtil;
 import com.video.server.domain.Chapter;
 import com.video.server.domain.ChapterExample;
 import com.video.server.dto.ChapterDto;
 import com.video.server.dto.PageDto;
 import com.video.server.mapper.ChapterMapper;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,23 +24,19 @@ public class ChapterService {
         //插件分页语句规则：调用startPage方法之后，对执行的第一个select语句会进行分页
         PageHelper.startPage(pageDto.getPage(),pageDto.getSize());//页码从1开始
         ChapterExample chapterExample = new ChapterExample();
+
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
         PageInfo<Chapter> pageInfo = new PageInfo(chapterList);
         pageDto.setTotal(pageInfo.getTotal());
-        List<ChapterDto> chapterDtoList = new ArrayList();
-        for (int i = 0; i < chapterList.size(); i++) {
-            Chapter chapter = chapterList.get(i);
-            ChapterDto chapterDto = new ChapterDto();
-            BeanUtils.copyProperties(chapter,chapterDto);
-            chapterDtoList.add(chapterDto);
-        }
+
+        //使用自己封装的 BeanUtils.copyProperties()
+        List<ChapterDto> chapterDtoList = CopyUtil.copyList(chapterList, ChapterDto.class);
         pageDto.setList(chapterDtoList);
     }
 
     public void save(ChapterDto chapterDto){
         chapterDto.setId(UuidUtil.getShortUuid());
-        Chapter chapter = new Chapter();
-        BeanUtils.copyProperties(chapterDto,chapter);
+        Chapter chapter = CopyUtil.copy(chapterDto, Chapter.class);
         chapterMapper.insert(chapter);
     }
 }
