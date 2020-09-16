8df9492e5028cf3bb66115fc157cb91c31cb6cca
ganshijian <1623487989@qq.com>
2020-09-16 15:32:41 +0800

    增加日志AOP,统一日志输出

--- business/src/main/java/com/video/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/video/business/controller/admin/ChapterController.java
@@ -22,6 +22,7 @@ import javax.annotation.Resource;
 @RequestMapping("/admin/chapter")
 public class ChapterController {
     private static final Logger LOG = LoggerFactory.getLogger(ChapterController.class);
+    public static final String BUSINESS_NAME = "大章";
 
     @Resource
     private ChapterService chapterService;
