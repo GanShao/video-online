42b57e6ed37bd7a6040a31648181269e15378821
ganshijian <1623487989@qq.com>
2020-09-23 15:13:13 +0800

    使用代码生成器重新生成大章Chapter的三层架构

--- business/src/main/java/com/video/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/video/business/controller/admin/ChapterController.java
@@ -1,6 +1,5 @@
 package com.video.business.controller.admin;
 
-
 import com.video.server.dto.ChapterDto;
 import com.video.server.dto.PageDto;
 import com.video.server.dto.ResponseDto;
@@ -8,45 +7,38 @@ import com.video.server.service.ChapterService;
 import com.video.server.util.ValidatorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
-//如果返回的是json数据用RestController（主要针对是接口），如果返回的是界面用Controller
 @RestController
 @RequestMapping("/admin/chapter")
 public class ChapterController {
+
     private static final Logger LOG = LoggerFactory.getLogger(ChapterController.class);
     public static final String BUSINESS_NAME = "大章";
 
     @Resource
     private ChapterService chapterService;
 
+    /**
+     * 列表查询
+     */
     @PostMapping("/query")
     public ResponseDto query(@RequestBody PageDto pageDto) {
-        //log日志输出，使用占位符{}
-        LOG.info("pageDto:{}", pageDto);
-
         ResponseDto responseDto = new ResponseDto();
         chapterService.query(pageDto);
         responseDto.setContent(pageDto);
         return responseDto;
     }
 
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
     @PostMapping("/save")
     public ResponseDto save(@RequestBody ChapterDto chapterDto) {
-        //log日志输出，使用占位符{}
-        LOG.info("chapterDto:{}", chapterDto);
-
-        //保存校验
-        ValidatorUtil.require(chapterDto.getName(), "名称");
-        ValidatorUtil.require(chapterDto.getCourseId(), "课程ID");
-        ValidatorUtil.length(chapterDto.getCourseId(), "课程ID", 1, 8);
+        // 保存校验
+        ValidatorUtil.length(chapterDto.getName(), "名称", 1, 50);
 
         ResponseDto responseDto = new ResponseDto();
         chapterService.save(chapterDto);
@@ -54,11 +46,11 @@ public class ChapterController {
         return responseDto;
     }
 
+    /**
+     * 删除
+     */
     @DeleteMapping("/delete/{id}")
     public ResponseDto delete(@PathVariable String id) {
-        //log日志输出，使用占位符{}
-        LOG.info("id:{}", id);
-
         ResponseDto responseDto = new ResponseDto();
         chapterService.delete(id);
         return responseDto;
--- server/src/main/java/com/video/server/service/ChapterService.java
+++ server/src/main/java/com/video/server/service/ChapterService.java
@@ -1,14 +1,14 @@
 package com.video.server.service;
 
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
-import com.video.server.util.CopyUtil;
-import com.video.server.util.UuidUtil;
 import com.video.server.domain.Chapter;
 import com.video.server.domain.ChapterExample;
 import com.video.server.dto.ChapterDto;
 import com.video.server.dto.PageDto;
 import com.video.server.mapper.ChapterMapper;
+import com.video.server.util.CopyUtil;
+import com.video.server.util.UuidUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -22,46 +22,49 @@ public class ChapterService {
     private ChapterMapper chapterMapper;
 
     /**
-     * 大章列表查询
-     * @param pageDto
+     * 列表查询
      */
-    public void query(PageDto pageDto){
-        //插件分页语句规则：调用startPage方法之后，对执行的第一个select语句会进行分页
-        PageHelper.startPage(pageDto.getPage(),pageDto.getSize());//页码从1开始
+    public void query(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
         ChapterExample chapterExample = new ChapterExample();
-
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
-        PageInfo<Chapter> pageInfo = new PageInfo(chapterList);
+        PageInfo<Chapter> pageInfo = new PageInfo<>(chapterList);
         pageDto.setTotal(pageInfo.getTotal());
-
-        //使用自己封装的 BeanUtils.copyProperties()
         List<ChapterDto> chapterDtoList = CopyUtil.copyList(chapterList, ChapterDto.class);
         pageDto.setList(chapterDtoList);
     }
 
     /**
-     * 大章列表保存方法
-     * @param chapterDto
+     * 保存，id有值时更新，无值时新增
      */
-    public void save(ChapterDto chapterDto){
+    public void save(ChapterDto chapterDto) {
         Chapter chapter = CopyUtil.copy(chapterDto, Chapter.class);
-        if(StringUtils.isEmpty(chapter.getId())){
+        if (StringUtils.isEmpty(chapterDto.getId())) {
             this.insert(chapter);
-        }else {
+        } else {
             this.update(chapter);
         }
     }
 
-    private void insert(Chapter chapter){
+    /**
+     * 新增
+     */
+    private void insert(Chapter chapter) {
         chapter.setId(UuidUtil.getShortUuid());
         chapterMapper.insert(chapter);
     }
 
-    private void update(Chapter chapter){
+    /**
+     * 更新
+     */
+    private void update(Chapter chapter) {
         chapterMapper.updateByPrimaryKey(chapter);
     }
 
-    public void delete(String id){
+    /**
+     * 删除
+     */
+    public void delete(String id) {
         chapterMapper.deleteByPrimaryKey(id);
     }
 }
